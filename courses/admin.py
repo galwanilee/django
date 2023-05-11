789eb2c787d38d1835bb530fb7470aad1236170d
OST <cpython.projects@gmail.com>
2023-05-11 08:35:16 +0300

    lecture 08_05_2023

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'is_current')
+    list_filter = ('start_date', 'end_date')
+    search_fields = ('name', 'description')
--- test_may_django/urls.py
+++ test_may_django/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from courses.views import course_list
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', course_list, name='courses'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
